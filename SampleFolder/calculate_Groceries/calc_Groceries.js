c2bc2b993e09bf91b5330124dd0fc108a1de6bbe
= <rwbutler1@icloud.com>
2025-08-04 15:50:44 -0400

    message

--- SampleFolder/calculate_Groceries/calc_Groceries.js
+++ SampleFolder/calculate_Groceries/calc_Groceries.js
@@ -4,8 +4,8 @@ let amount3;
 
 function calculateTotal() {
  amount1 = parseFloat(document.getElementById('amount1').value);
- amount1 = parseFloat(document.getElementById('amount2').value);
- amount1 = parseFloat(document.getElementById('amount3').value);
+ amount2 = parseFloat(document.getElementById('amount2').value);
+ amount3 = parseFloat(document.getElementById('amount3').value);
 
 
 let total = amount1 + amount2 + amount3;
